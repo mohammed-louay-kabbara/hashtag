96e023897e597ae598e0a5ae48cd2d80ee7f44a7
Doha05a <mohamedlouayk@gmail.com>
2025-10-10 15:21:23 +0300

    upload laravel nnnn

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -12,6 +12,7 @@ class NotificationController extends Controller
 
     public function index()
     {
+        dd(Auth::id());
        $notification=notification::where('id',Auth::id())->with(['user','sender'])->orderByDesc('created_at')->get();
         return response()->json($notification, 200);
     }
